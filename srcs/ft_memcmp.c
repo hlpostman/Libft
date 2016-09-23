cc663fff392c75c156dd4ded950810d915440c92
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-23 12:38:13 -0700

    Fix norm error on ft_memcmp.c where the declarations were bad aligned (one too few tabs between size_t and i)

--- srcs/ft_memcmp.c
+++ srcs/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 11:47:48 by hpostman          #+#    #+#             */
-/*   Updated: 2016/09/23 12:13:30 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/09/23 12:37:10 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char	*buffer1;
 	const unsigned char	*buffer2;
-	size_t			i;
+	size_t				i;
 
 	buffer1 = (unsigned char *)s1;
 	buffer2 = (unsigned char *)s2;
