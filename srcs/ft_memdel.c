144a5d654d6e49fb17e6d8afe322be65d221b6f7
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-23 12:28:47 -0700

    Implement ft_memdel.c. NOT YET TESTED.

---
+++ srcs/ft_memdel.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/23 12:20:23 by hpostman          #+#    #+#             */
+/*   Updated: 2016/09/23 12:28:26 by hpostman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Non-standard function memdel.  Takes the address of a memory area that needs
+** to be freed, uses free(3) to free that area, AND points the pointer taken
+** in as an argument to NULL. Does not returnin anything.
+*/
+
+#include "libft.h"
+
+void ft_memdel(void **ap)
+{
+	free(*ap);
+	*ap = NULL;
+}
