ccc595c44fdc109808455b576e9b3ec1733af49c
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-23 11:19:20 -0700

    Implement ft_memchr.c. NOT YET TESTED.

---
+++ srcs/ft_memchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/23 11:09:19 by hpostman          #+#    #+#             */
+/*   Updated: 2016/09/23 11:18:56 by hpostman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Re-coding of the Standard C Library function memchr(3).
+** Parameters: void pointer to some read-only source memory, integer "c" that
+** is probably in ASCII or extended ASCII range and will get cast to an
+** unsigned char type inside the function, and a number "n" of type size_t
+** Side effect: Locates the first occurrence of (unsigned char)c in memory
+** at s + 0 through s + (n - 1)
+** Return value: pointer to the bytelocated, or NULL if no byte containing
+** (unsigned char)c exists in the first n bytes of the source.
+*/
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*buffer;
+	size_t			i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (buffer[i] == (unsigned char)c)
+			return (buffer + i);
+		i++;
+	}
+	return (NULL);
+}
