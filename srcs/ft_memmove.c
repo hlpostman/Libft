10995edd7ea1047f44ca3da1b0b0407de7e8eea2
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-23 14:58:49 -0700

    Free temp_buffer and check that malloc call worked in ft_memmove.c

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 09:56:44 by hpostman          #+#    #+#             */
-/*   Updated: 2016/09/23 14:47:11 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/09/23 14:58:08 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,11 @@
 ** pointer to some read-only source memory, and a number "len" of type size_t.
 ** Side-effect: copies len bytes from the source memory (so source + 0 through
 ** source + (len - 1)) to the destination.
-** Return value: pointer to the front of destination.
+** Return value: pointer to the front of destination (or NULL if copy failed).
 ** The manual page says "The two strings may overlap; th copy is always done
-** in a non-destructive manner" - I'm not sure what that means.  I guess that
-** if the destination memory is somewhere in 1 - (len - 1) of the source bytes,
-** the function makes sure to remember what was there in source so that it can
-** remember what it needs to copy later on when that space has been overwrriten
-** in the destination?  but that doesn't really make sense because the source
-** memory is specified as read-only anyway. But it seems like this is how others
-** have interpreted it...hm... Doing it that way for now.
+** in a non-destructive manner" - since src is const, any overlap with dst
+** will only get modified indirectly, with the dst pointer, not directly
+** with the src pointer.
 */
 
 #include "libft.h"
@@ -34,8 +30,12 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	unsigned char	*temp_buffer;
 
 	temp_buffer = (unsigned char *)malloc(len);
-	ft_memcpy(temp_buffer, src, len);
-	ft_memcpy(dst, temp_buffer, len);
-	free(temp_buffer);
-	return (dst);
+	if (temp_buffer)
+	{
+		ft_memcpy(temp_buffer, src, len);
+		ft_memcpy(dst, temp_buffer, len);
+		free(temp_buffer);
+		return (dst);
+	}
+	return (NULL);
 }
