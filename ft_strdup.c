ecf4797649be188fb2713fc2ee0fec916fdf250b
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-24 08:12:09 -0700

    Implement ft_strdup.c. NOT YET TESTED.

---
+++ ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/24 07:29:48 by hpostman          #+#    #+#             */
+/*   Updated: 2016/09/24 08:11:36 by hpostman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Re-coding of the Standard C Library function strdup(3).
+** Paramters: const string
+** Side effects: allocates the right amount of memory for a copy of the
+** argument string to be made, and copies the argument string into that memory
+** Return value: a pointer to the front of the new memory block holding
+** the copy of the argument string, OR, if not enough memory was available
+** and the memory allocation failed, NULL.
+** Much like strcpy(3), but allocates memory for the result, whereas with
+** strcpy(3), the memory area for the result is passed as the first argument.
+*/
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*s_duplicate;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s);
+	s_duplicate = (char *)ft_memalloc(sizeof(size_t) * (len + 1));
+	s_duplicate = (char *)ft_memcpy(s_duplicate, s, len);
+	return (s_duplicate);
+}
