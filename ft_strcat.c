cd0c4abc505b804f7423c28224bb0c50a4c8f4ee
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-24 10:01:58 -0700

    Change parameter names from s1 and s2 to dst and src since it is not a comparison and is a src dst situation and this is consistent with my other functions of this nature though diverges from exact prototype varaible names in the manual page for strcat, I have realized that the manual pages are not edited for consistency across all entries so I'm going to be consistent within my own work rather than consistent to a source that doesn't have a precise standard, for ease

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,30 +6,30 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 09:08:17 by hpostman          #+#    #+#             */
-/*   Updated: 2016/09/24 09:27:40 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/09/24 09:59:01 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ** Re-coding of the Standard C Library function strcat(3).
 ** Paramters: two nul-terminated strings, the second one a const
-** Side effect: appends a copy of s2 to s1, starting at (overwriting) the
-** nul terminator of s1, assuming s1 has sufficient space to hold the
+** Side effect: appends a copy of src to dst, starting at (overwriting) the
+** nul terminator of dst, assuming dst has sufficient space to hold the
 ** concatenation.
-** Return value: pointer to s1
-** Behavior in the case of overlap between s1 and s2 is undefined, like
+** Return value: pointer to dst
+** Behavior in the case of overlap between dst and src is undefined, like
 ** in memcpy.
 */
 
 #include "libft.h"
 
-char	*ft_strcat(char *s1, const char *s2)
+char	*ft_strcat(char *dst, const char *src)
 {
-	size_t	s1_len;
-	char	*end_of_s1;
+	size_t	dst_len;
+	char	*end_of_dst;
 
-	s1_len = ft_strlen(s1);
-	end_of_s1 = s1 + (sizeof(char) * s1_len);
-	s1 = ft_strcpy(end_of_s1, s2);
-	return (s1);
+	dst_len = ft_strlen(dst);
+	end_of_dst = dst + (sizeof(char) * dst_len);
+	dst = ft_strcpy(end_of_dst, src);
+	return (dst);
 }
