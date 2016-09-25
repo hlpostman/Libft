e6be8b21b7e251fb31f9ce577c88790eedc06b51
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-25 15:44:56 -0700

    Add non-standard string functions and relevant macros to header file libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 15:10:47 by hpostman          #+#    #+#             */
-/*   Updated: 2016/09/24 21:01:06 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/09/25 15:26:17 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 # define LIBFT_H
 # include <string.h>
 # include <stdlib.h>
-# define IS_WHITESPACE(s) s == ' ' || s == '\n' || s == '\t'
+# define IS_WHITESPACE(c) c == ' ' || c == '\n' || c == '\t'
+# define TRIM(c) c == ' ' || c == ',' || c == '\n' || c == '\t'
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -47,5 +48,17 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
 
 #endif
