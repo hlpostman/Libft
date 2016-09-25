c1899c0c0b0cb4697c99f573536703c79162d71f
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-25 09:47:12 -0700

    Implement ft_strclr.c.  NOT YET TESTED.

---
+++ ft_strclr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strclr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/25 09:39:39 by hpostman          #+#    #+#             */
+/*   Updated: 2016/09/25 09:46:43 by hpostman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Coding of non-standard function strclr().
+** Parameters: a string that needs to be cleared
+** Side effect: sets every byte in the string to 0, equivalent of '\0' as per
+** this site: www.quora.com/What-is-the-difference-between-0-and-null-
+** terminator-in-a-C-string
+** Return value: none
+*/
+
+#include "libft.h"
+
+void	ft_strclr(char *s)
+{
+	ft_bzero((void *)s, ft_strlen(s));
+}
