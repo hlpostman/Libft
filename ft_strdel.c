e72a7236b6b277b869400dafe3ca2069ba8cc180
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-25 09:39:17 -0700

    Implement ft_strdel.c. NOT YET TESTED.

---
+++ ft_strdel.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/25 09:29:40 by hpostman          #+#    #+#             */
+/*   Updated: 2016/09/25 09:36:30 by hpostman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Coding of non-standard function strdel().
+** Parameters: address of a string that needs to be freed and have its pointer
+** set to NULL
+** Side effect: frees the address pointed to and sets that address to NULL
+** Return value: none
+*/
+
+#include "libft.h"
+
+void	ft_strdel(char **as)
+{
+	ft_memdel((void *)as);
+}
