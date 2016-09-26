7db3a18371e1036c52a478093905e4dad8c3bb46
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-09-26 07:31:16 -0700

    Take out misguided casts in ft_isalpha, and change a true result to return 1 not c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 20:11:54 by hpostman          #+#    #+#             */
-/*   Updated: 2016/09/24 20:17:39 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/09/26 07:30:28 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,14 @@
 ** Side effect: none
 ** Return value: zero if the unsigned char cast of c is in neither the capital
 ** alphabet A-Z or lowercase alphabet a-z, non-zero otherwise (in this
-** implementation, it will return c)
+** implementation, it will return 11111111111)
 */
 
 #include "libft.h"
 
 int	ft_isalpha(int c)
 {
-	if (((unsigned char)c >= 'A' && (unsigned char)c <= 'Z') ||
-			((unsigned char)c >= 'a' && (unsigned char)c <= 'z'))
-		return (c);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
 	return (0);
 }
