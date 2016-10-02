45e81fadc9614a7190774a85e2d2f3dcc11dc261
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-10-01 22:18:15 -0700

    Make sure the next starting place is the end of the last substring in ft_strsplit. Do a few norm things - shorten number of lines in the function, and shorten variable names to make lines shorter than the max norm length.  ALSO added protection for if the pointer passed for a string is NULL.

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/27 14:16:05 by hpostman          #+#    #+#             */
-/*   Updated: 2016/10/01 21:50:46 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/10/01 22:15:45 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,34 +23,31 @@
 
 #include "libft.h"
 
-char			**ft_strsplit(char const *s, char c)
+char	**ft_strsplit(char const *s, char c)
 {
 	char	**result;
 	size_t	num_words;
-	size_t	start_str;
-	size_t	end_str;
+	size_t	start;
+	size_t	end;
 	size_t	i;
 
-	if (s)
+	num_words = ft_count_delim(s, c);
+	result = (char **)ft_memalloc(sizeof(char *) * (num_words + 1));
+	if (!s || result == NULL)
+		return (NULL);
+	start = 0;
+	end = 0;
+	i = 0;
+	while (s && i < num_words)
 	{
-		num_words = ft_count_delim(s, c);
-		result = (char **)ft_memalloc(sizeof(char *) * (num_words + 1));
-		if (result == NULL)
-			return (NULL);
-		start_str = 0;
-		end_str = 0;
-		i = 0;
-		while (i < num_words)
-		{
-			while (s[start_str++] == c)
-				;
-			end_str = start_str;
-			while (s[end_str++] != c)
-				;
-			result[i] = ft_strsub(s, start_str - 1, (end_str - start_str - 1) + 1);
-			i++;
-		}
-		return (result);
+		while (s && s[start++] == c)
+			;
+		end = start;
+		while (s && s[end++] != c)
+			;
+		result[i] = ft_strsub(s, start - 1, (end - start - 1) + 1);
+		start = end;
+		i++;
 	}
-	return (NULL);
+	return (result);
 }
