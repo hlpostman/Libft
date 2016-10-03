9c6cc74b7b31eb65d3a98ce681d93719450ef20d
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-10-02 19:20:12 -0700

    Change ft_isalnum.c to return 1 in the case of true

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 20:18:25 by hpostman          #+#    #+#             */
-/*   Updated: 2016/09/24 20:22:21 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/10/02 19:19:19 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 ** Parameters: integer c, likely in ASCII or extended ASCII range
 ** Side effect: none
 ** Return value: 0 if the unsigned char cast of c is neither an upper- or
-** lower-case letter nor digit, or if it is, then c itself is returned
+** lower-case letter nor digit, or if it is, then 1 is returned in this
+** implementation - another route would be to return c
 */
 
 #include "libft.h"
@@ -23,6 +24,6 @@
 int	ft_isalnum(int c)
 {
 	if (ft_isalpha(c) || ft_isdigit(c))
-		return (c);
+		return (1);
 	return (0);
 }
