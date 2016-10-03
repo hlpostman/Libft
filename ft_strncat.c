0951cd6f87101d4c97cd86217898d5c7b7192482
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-10-02 17:49:43 -0700

    Make sure ft_strncat.c only copies to the length of the source if that is less than n.  Explicitly set null terminator

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 09:44:25 by hpostman          #+#    #+#             */
-/*   Updated: 2016/09/26 07:11:58 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/10/02 17:47:17 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,12 @@ char	*ft_strncat(char *dst, const char *src, size_t n)
 {
 	size_t	dst_len;
 	char	*end_of_dst;
+	size_t	checkto;
 
 	dst_len = ft_strlen(dst);
 	end_of_dst = dst + (sizeof(char) * dst_len);
-	dst = (char *)ft_memcpy(dst + dst_len, src, n);
+	checkto = ft_min(ft_strlen(src), n);
+	dst = (char *)ft_memcpy(dst + dst_len, src, checkto);
+	*(dst + checkto) = '\0';
 	return (dst - dst_len);
 }
