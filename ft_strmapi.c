aac1977f08713b244fb918540aacc5fc6674c33e
Hannah-Lily Postman <hpostman@student.42.us.org>
2016-10-08 15:41:41 -0700

    Make ft_strmapi.c like ft_strmap.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 12:46:38 by hpostman          #+#    #+#             */
-/*   Updated: 2016/10/02 20:13:23 by hpostman         ###   ########.fr       */
+/*   Updated: 2016/10/08 15:41:21 by hpostman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,18 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*new_s;
 	size_t	i;
+	size_t	len;
 
 	if (!s || !f)
 		return (NULL);
-	new_s = ft_strdup(s);
-	if (!new_s)
+	len = ft_strlen(s);
+	new_s = ft_strnew(len);
+	if (new_s == NULL)
 		return (NULL);
 	i = 0;
 	while (new_s[i])
 	{
-		new_s[i] = f(i, new_s[i]);
+		new_s[i] = f(i, s[i]);
 		i++;
 	}
 	return (new_s);
